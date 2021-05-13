348207d0251f9cff2bab278bd4d5152729720bc2
John Watson <johnwatson484@hotmail.com>
2021-05-13 08:48:13 +0100

    Promise added to connection

--- app/api/relay.js
+++ app/api/relay.js
@@ -3,39 +3,44 @@ const { responseItemToString } = require('./utils')
 const { hostname, keyRule, key } = require('../config').relay
 
 async function get (path) {
-  https.get({
-    hostname: hostname,
-    path: (!path || path.length === 0 || path[0] !== '/' ? '/' : '') + path,
-    port: 443,
-    headers: {
-      ServiceBusAuthorization:
-            https.createRelayToken(https.createRelayHttpsUri(hostname, path), keyRule, key)
-    }
-  }, (res) => {
-    if (res.statusCode !== 200) {
-      console.error(`Request Failed.\n Status Code: ${res.statusCode}`)
-      res.resume()
-    } else {
-      let payload
-      res.setEncoding('utf8')
-      res.on('data', (chunk) => {
-        console.log(`BODY: ${chunk}`)
-        payload += chunk
+  const promise = new Promise((resolve) => {
+    https.get({
+      hostname: hostname,
+      path: (!path || path.length === 0 || path[0] !== '/' ? '/' : '') + path,
+      port: 443,
+      headers: {
+        ServiceBusAuthorization: https.createRelayToken(https.createRelayHttpsUri(hostname, path), keyRule, key)
+      }
+    }, (res) => {
+      if (res.statusCode !== 200) {
+        console.error(`Request Failed. Status Code: ${res.statusCode}`)
+        res.resume()
+        resolve({
+          error: `Request Failed. Status Code: ${res.statusCode}`
+        })
+      } else {
+        let data
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`)
+          data += chunk
+        })
+        res.on('end', () => {
+          console.log('No more data in response.')
+          resolve({
+            headers: responseItemToString(res.headers),
+            payload: responseItemToString(data)
+          })
+        })
+      }
+    }).on('error', (e) => {
+      console.error(`Got error: ${e.message}`)
+      resolve({
+        error: e.message
       })
-      res.on('end', () => {
-        console.log('No more data in response.')
-        return {
-          headers: responseItemToString(res.headers),
-          payload: responseItemToString(payload)
-        }
-      })
-    }
-  }).on('error', (e) => {
-    console.error(`Got error: ${e.message}`)
-    return {
-      error: e.message
-    }
+    })
   })
+  return await promise
 }
 
 module.exports = {
