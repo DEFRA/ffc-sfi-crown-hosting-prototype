b3045b994421c837d00e1f574b39cf0dc6d21fc7
John Watson <johnwatson484@hotmail.com>
2021-05-24 21:49:20 +0100

    Cookies disabled

--- app/api/index.js
+++ app/api/index.js
@@ -1,13 +1,14 @@
 const { get, post } = require('./standard')
 const { get: getRelay } = require('./relay-http')
+const { getRelayToken } = require('./utils')
 
 async function request (query) {
-  // if (query.relay && query.relayKey) {
-  //   query.token = getRelayToken(query.relayNamespace, query.relayKeyName, query.relayKey)
-  // }
-  if (query.relay) {
-    return getRelay(query)
+  if (query.relay && query.relayKey) {
+    query.token = getRelayToken(query.relayNamespace, query.relayKeyName, query.relayKey)
   }
+  // if (query.relay) {
+  //   return getRelay(query)
+  // }
   return query.method === 'post' ? post(query.url, query.data, query.token) : get(query.url, query.token)
 }
 
--- app/server.js
+++ app/server.js
@@ -18,13 +18,13 @@ async function createServer () {
   })
 
   // Set cookie properties
-  server.state('ffc_sfi_chp', config.cookieOptions)
+  // server.state('ffc_sfi_chp', config.cookieOptions)
 
   // Register the plugins
   await server.register(require('@hapi/inert'))
   await server.register(require('./plugins/views'))
   await server.register(require('./plugins/router'))
-  await server.register(require('./plugins/crumb'))
+  // await server.register(require('./plugins/crumb'))
   await server.register(require('./plugins/error-pages'))
   await server.register(require('blipp'))
   await server.register(require('./plugins/logging'))
